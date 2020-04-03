4e03ec0baad39cf1fa2e997cb293e7c6bc60bfec
cartaxo <cartaxo@bionexo.com>
2020-04-03 13:08:44 -0300

    Running the store

--- lib/main.dart
+++ lib/main.dart
@@ -27,38 +27,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return WebviewScaffold(
+      url: "https://www.lojaumdois.com.br",
+      withJavascript: true,
+      withZoom: false,
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        title: Text("umdois"),
+        elevation: 1
       ),
     );
   }
